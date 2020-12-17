b4ae78fa608e031230115fd9889bdde68f2b3952
demotesting221198 <75855474+demotesting221198@users.noreply.github.com>
2020-12-17 15:31:01 +0530

    Update Appium.java

--- src/test/java/appiumtests/Appium.java
+++ src/test/java/appiumtests/Appium.java
@@ -23,6 +23,7 @@ public class Appium {
 		      DesiredCapabilities caps = new DesiredCapabilities();
 			 String username = System.getenv("BROWSERSTACK_USERNAME");
 			 String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+			 String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
 		      
 		      
 		      // Set URL of the application under test
